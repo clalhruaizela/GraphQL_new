19e2a5a99ed8b09498ff6e1273d7ae353e96b250
Mapuia <clalhruaizela07@gmail.com>
2024-11-05 01:33:52 +0530

    zan

--- Anime/src/home/anime_Home.tsx
+++ Anime/src/home/anime_Home.tsx
@@ -6,18 +6,26 @@ import { useState } from "react";
 import { formatDuration } from "./utilties/duration";
 import { Card, CardHeader } from "@/components/ui/card";
 import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+} from "@/components/ui/pagination";
+import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { useSearchParams } from "react-router-dom";
 
 const AnimeHome = () => {
   const graphql = graphqlClient();
-  const [page, setPage] = useState(1);
-  const [hoverId, setHoverId] = useState(false);
-
+  const [hoverId, setHoverId] = useState<number | null>(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = parseInt(searchParams.get("page") || "1");
   const { isLoading, isError, data } = useQuery({
-    queryKey: [],
+    queryKey: ["animePage", page],
     queryFn: async () => {
       return await graphql.request(GET_ANIME_BY_ID, {
         page,
@@ -25,21 +33,34 @@ const AnimeHome = () => {
         lastPage: 20,
       });
     },
+    keepPreviousData: true,
   });
+
+  const handlePageChange = (page: number) => {
+    setSearchParams(
+      (param) => {
+        param.set("page", page.toString());
+        return param;
+      },
+      {
+        preventScrollReset: true,
+      }
+    );
+  };
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error</div>;
   // console.log(data);
   const totalPages = data?.Page?.pageInfo?.lastPage || 1;
   return (
     <div className="min-w-full min-h-screen bg-slate-900">
-      <div className="grid grid-cols-6 w-9/12 mx-auto ">
-        <div className="col-span-4 ">
-          <div className="grid grid-cols-4 gap-4  ">
+      <div className="lg:grid lg:grid-cols-6 lg:w-9/12 lg:mx-auto ">
+        <div className="lg:col-span-4 ">
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mx-4 ">
             {/* <div className="flex flex-"> */}
             {data?.Page?.media?.map((anime: any) => (
               <Card
                 key={anime.id}
-                className=" flex flex-col justify-center items-center text-white border-none bg-slate-900"
+                className=" flex flex-col justify-center  text-white border-none bg-slate-900"
               >
                 <CardHeader>
                   <Popover>
@@ -48,23 +69,23 @@ const AnimeHome = () => {
                         src={anime.coverImage?.large}
                         alt={anime.title?.english}
                         width="100%"
+                        onMouseEnter={() => setHoverId(anime.id)}
+                        onMouseLeave={() => setHoverId(null)}
                       />
                     </PopoverTrigger>
-                    <PopoverContent>
-                      <h2 className="font-bold"> {anime.title?.english} </h2>
-                      <div>Status: {anime.status} Airing </div>
-                    </PopoverContent>
+                    {hoverId === anime.id && (
+                      <PopoverContent className="">
+                        <h2 className="font-bold"> {anime.title?.english} </h2>
+                        <div>Status: {anime.status} Airing </div>
+                      </PopoverContent>
+                    )}
                   </Popover>
                 </CardHeader>
-
-                <div className="flex flex-col w-60 ">
+                <div className="flex flex-col justify-center text-sm">
                   <div>
-                    <div className="truncate ...">
-                      {" "}
-                      Title: {anime.title?.english}
-                    </div>
+                    <div className="truncate">{anime.title?.english}</div>
                   </div>
-                  <div className="flex flex-row gap-6">
+                  <div className="flex  gap-2">
                     <div>{anime.format}</div>
                     <div>
                       {anime.duration ? formatDuration(anime.duration) : "N/A"}
@@ -76,23 +97,20 @@ const AnimeHome = () => {
           </div>
           {/* </div> */}
         </div>
-        <div className=" col-span-2 flex flex-col  items-center w-9/12  bg-slate-700 ">
+        <div className=" lg:col-span-2 flex flex-col  items-center lg:w-9/12  bg-slate-700 ">
           {data?.Page?.media?.map((anime: any) => (
-            <div key={anime.id} className=" w-80 ">
-              <Card className="flex flex-row bg-slate-700 border-l-0 border-r-0 rounded-none border-gray-600 text-white">
-                <CardHeader>
+            <div key={anime.id} className=" w-full ">
+              <Card className="flex flex-row bg-slate-700 border-l-0 border-r-0  rounded-none border-gray-600 text-white">
+                <div className="py-4 pl-14">
+                  <img
+                    src={anime.coverImage?.large}
+                    alt={anime.title?.english}
+                    width="80%"
+                  />
+                </div>
+                <div className="flex flex-col pt-5 pl-2 text-sm">
                   <div>
-                    <img
-                      src={anime.coverImage?.large}
-                      alt={anime.title?.english}
-                      height={260}
-                      width="100%"
-                    />
-                  </div>
-                </CardHeader>
-                <div className="flex flex-col pt-10 text-sm">
-                  <div>
-                    <div className="w-60">{anime.title?.english}</div>
+                    <div className="w-40">{anime.title?.english}</div>
                   </div>
                   <div>
                     <div>{anime.format}</div>
@@ -110,7 +128,40 @@ const AnimeHome = () => {
               </Card>
             </div>
           ))}
-          <div></div>
+          <div>
+            {totalPages > 1 && (
+              <Pagination>
+                <PaginationContent>
+                  {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+                    (item) => {
+                      if (
+                        item === 1 ||
+                        item === totalPages ||
+                        Math.abs(item - page) <= 2
+                      ) {
+                        return (
+                          <PaginationItem key={item}>
+                            <PaginationLink
+                              isActive={item === page}
+                              onClick={() => {
+                                window.scrollTo(0, 0);
+                                handlePageChange(item);
+                              }}
+                            >
+                              {item}
+                            </PaginationLink>
+                          </PaginationItem>
+                        );
+                      } else if (item === page - 3 || item === page + 3) {
+                        return <PaginationEllipsis key={item} />;
+                      }
+                      return null;
+                    }
+                  )}
+                </PaginationContent>
+              </Pagination>
+            )}
+          </div>
         </div>
       </div>
     </div>
